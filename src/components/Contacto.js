83545ca5341b6773fc61ce54f3585476b8e042d2
Tiago1704 <36915137+Tiago1704@users.noreply.github.com>
2020-08-22 12:23:42 -0300

    Update Contacto.js
    Agregado email-js api

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -1,7 +1,44 @@
+
 import React, { Component } from 'react'
 
+// importar emailjs para enviar correo
+import emailjs from "emailjs-com";
+//importar swal para popUps
+import swal from 'sweetalert';
+
+
+
+
 export default class Contacto extends Component {
+
+
+
   render() {
+
+    /* funcion encargada de enviar un email  a traves de emailJs.
+     'contacto_beertech' : ID DEL SERVICIO
+     'datos_contacto' : ID PLANTILLA DE EMAIL
+     'user_oegYyvxy57UCNV5jeXonc' : USER ID DE EMAILJS (ACCOUNTS -> Api Keys : User ID) */
+    function sendEmail(e) {
+      e.preventDefault();
+
+      emailjs.sendForm('contacto_beertech', 'datos_contacto', e.target, 'user_oegYyvxy57UCNV5jeXonc')
+        .then((result) => {
+          swal({
+            title: "Mensaje enviado con exito",
+            icon: "success",
+            button: "Cerrar",
+          });
+
+        }, (error) => {
+          swal("Error al enviar el mensaje", "Intentelo nuevamente", "error");
+        });
+        e.target.reset()
+
+
+
+    }
+
     return(
       <main class="contenedor">
         <h2 class="centrar-texto">Contacto</h2>
@@ -12,21 +49,21 @@ export default class Contacto extends Component {
             </div>
 
             <div class="columnas-10 formulario-contacto">
-                <form action="#">
+                <form onSubmit={sendEmail}>
                     <div class="campo">
                         <label for="nombre">Nombre</label>
-                        <input type="text" id="nombre" placeholder="Tu Nombre" />
+                        <input type="text" id="nombre" placeholder="Tu Nombre" name="name" />
                     </div>
                     <div class="campo">
                         <label for="email">E-Mail</label>
-                        <input type="email" id="email" placeholder="Tu Correo Electrónico" />
+                        <input type="email" id="email" placeholder="Tu Correo Electrónico" name="email" />
                     </div>
                     <div class="campo mensaje">
                         <label for="mensaje">Mensaje</label>
-                        <textarea id="mensaje"></textarea>
+                        <textarea id="mensaje" name="message"></textarea>
                     </div>
                     <div class="campo enviar">
-                        <input type="submit" value="Enviar" class="btn btn-primario" />
+                        <input type="submit" value="Enviar Mail" class="btn btn-primario" />
                     </div>
                 </form>
             </div>
